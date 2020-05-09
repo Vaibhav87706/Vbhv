dc25f27b25a404e66fd0e47ffa43adf3c7a22f38
Vaibhav87706 <63184819+Vaibhav87706@users.noreply.github.com>
2020-05-09 15:59:00 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+<html>
+<center><body bgcolor="#ffff66">
+<br><br><br>
+<font size ="6">
+<font color="#ff0000">
+<br>
+<?php
+  
+$name=$_POST["name"]; 
+$password=$_POST["password"];  
+  
+echo "<b>ID: $name<br> 
+<b>your password is: $password"; 
+
+
+echo"\n <br><br><br><br><br><br>Register Sucessfully";  
+?>
+<br><br>
+</html>
+</body>  
